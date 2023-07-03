275be89475d5ef44fb7be8c9f8062762d4c84703
zjmitxwz <3222690362@qq.com>
2023-07-03 15:52:02 +0800

    v2

--- main.py
+++ main.py
@@ -1,43 +1,335 @@
-import tools
 import os
-import sys
-import random
-import shutil
-import json
 import queue
+import sys
+import logging
+import tools
+import shutil
+import time
+import logging.handlers
+import socket
+import json
+import threading
+import ftplib
 from paho.mqtt import client as mqttclient
-import TeskThread
-import MyFtp
-config = ""
+
+lock = threading.Lock()
+# 创建一个任务队列
 tesk_queue = queue.Queue()
 
 
+#####################class#######################
+class Log():
+    def __init__(self,path):
+        self.logger = logging.getLogger()
+        self.fh = logging.handlers.TimedRotatingFileHandler(os.path.join(path,"log/run.log"),"D",1,7,'utf-8')
+        self.formatter = logging.Formatter("%(asctime)s   %(levelname)s   %(message)s")
+        self.fh.setFormatter(self.formatter)
+        self.logger.addHandler(self.fh)
+        self.logger.setLevel(logging.DEBUG)
+    def log(self,level,n) -> None:
+        if(level=="info"):
+            self.logger.info(n)
+        elif(level=="error"):
+            self.logger.error(n)
+        elif(level=="warning"):
+            self.logger.warning(n)
+        else:
+            self.logger.critical(n)
+
+class Myftp():
+    def __init__(self,ip,port,user,pwd) -> None:
+        self.ip = ip
+        self.ftp = ftplib.FTP()
+        self.ftp.encoding='utf-8'
+        self.port = port
+        self.user = user
+        self.pwd = pwd
+        self.status = -1
+    def connect(self):
+        try:
+            self.ftp.connect(self.ip,self.port)
+            self.ftp.login(self.user,self.pwd)
+        except ConnectionRefusedError as e:
+            return {"code":-1,"re":e.strerror}
+        self.status = 0
+        return {"code":0,"re":"服务器链接成功"}
+    def if_c(self):
+        if(self.status==-1):
+            return {"code":-1,"re":"ftp服务器为链接"}
+        try:
+            self.ftp.nlst()
+        except (ConnectionAbortedError,AttributeError) as e:
+            self.status = -1
+            return {"code":0,"re":e.strerror}
+
+    def dow(self,list):
+        ok = []
+        err = []
+
+        for i in list:
+            if(os.path.splitext(i)[-1]!=""):
+                t = self._dowfile(i)
+                if(t["code"]==0):
+                    ok.append(t)
+                else:
+                    err.append(t)
+            else:
+                t = self._dowfree(i)
+                if(t["code"]==0):
+                    ok.append(t)
+                else:
+                    err.append(t)
+        return {"code":0,"ok":ok,"err":err}
+    def _dowfile(self,file):
+        ftp_p = os.path.split(file)[0]
+        try:
+            list = self.ftp.nlst(ftp_p)
+        except ftplib.error_perm as e:
+            print("服务器不存在这个路径")
+            return {"code":-1,"re":"服务器不存在这个路径"}
+        if file not in list:
+            print("服务器不存在这个文件")
+            return {"code":-1,"re":"服务器不存在这个文件"}
+        path = Pgm_env.data_path
+        for i in ftp_p.split("/"):
+            path = os.path.join(path,i) 
+        file_n = os.path.split(file)[-1]
+        if(not os.path.exists(path)):
+            os.makedirs(path)
+        path = os.path.join(path,file_n)
+        buffer_size = 102400
+        with open(path,"wb+") as f:
+            self.ftp.retrbinary("RETR {}".format(file),f.write,buffer_size)
+            f.flush()
+        print("文件:{}下载完毕".format(file_n))
+        return {"code":0,"re":"文件:{}下载完毕".format(file_n),"filename":file_n}
+    def _dowfree(self,free):
+        try:
+            list = self.ftp.nlst(free)
+        except:
+            print("服务器不存在这个目录{}".format(free))
+            return {"code":-1,"re":"服务器不存在这个目录"}
+        s = free.split("/")
+        path = Pgm_env.data_path
+        for i in s:
+            path = os.path.join(path,i)
+        if(not os.path.exists(path)):
+            os.makedirs(path)
+        for i in list:
+            if(os.path.splitext(i)[-1]!=""):
+                self._dowfile(i)
+            else:
+                self._dowfree(i)
+        return {"code":0,"re":"目录下载完毕"}
+    
+
+    def up(self,list):
+        ok = []
+        err = []
+        for i in list:
+            if(os.path.splitext(i)[-1]!=""):
+                t = self._upfile(i)
+                if(t["code"]!=0):
+                    err.append(t)
+                else:
+                    ok.append(t)
+            else:
+                t = self._upfree(i)
+                if(t["code"]!=0):
+                    err.append(t)
+                else:
+                    ok.append(t)
+        return {"code":0,"re":"完成","ok":ok,"err":err}
+    def _upfile(self,file):
+        p = file.split("/")
+        path = Pgm_env.data_path
+        for i in p:
+            path = os.path.join(path,i)
+        if(not os.path.exists(path)):
+            print("文件{}不存在".format(file))
+            return {"code":-1,"re":"文件不存在","filename":"{}".format(os.path.split(file)[-1])}
+        s = os.path.split(file)[0]
+        s = s.split("/")
+        f_p = "/"
+        for i in s:
+            f_p = os.path.join(f_p,i)
+            try:
+                self.ftp.cwd(f_p)
+            except:
+                self.ftp.mkd(f_p) 
+        bufsiz=10240
+        with open(path,"rb") as f:
+            self.ftp.storbinary("STOR {}".format(os.path.join(f_p,os.path.split(file)[-1])),f,bufsiz)
+        return {"code":0,"re":"文件上传成功"}
+        
+    def _upfree(self,free):
+        p = free.split("/")
+        path = Pgm_env.data_path
+        for i in p:
+            path = os.path.join(path,i)
+        print(path)
+        if(not os.path.exists(path)):
+            return {"code":-1,"re":"U盘不存在这个目录"}
+        f_p = "/"
+        for i in p:
+            f_p = os.path.join(f_p,i)
+            try:
+                self.ftp.cwd(f_p)
+            except:
+                self.ftp.mkd(f_p)
+            
+        list = os.listdir(path)
+        for i in list:
+            if(os.path.splitext(i)[-1]!=""):
+                self._upfile(os.path.join(free,i))
+            else:
+                self._upfree(os.path.join(free,i))
+        return {"code":0,"re":"文件夹上传成功"}
+
+    
+class Pgm_env():
+    data_path = "/root/code/data"
+    data_disk = "/dev/mmcblk0p4"
+    pgm_path = "/root/code"
+    thread_status = 0
+    log = None
+    config = ""
+class TeskThread(threading.Thread):
+    def __init__(self,client,tesk_queue,ftp):
+        threading.Thread.__init__(self)
+        self.client = client
+        self.tesk_queue = tesk_queue
+        self.ftp = ftp
+    def run(self):
+        tools.umount(Pgm_env.data_disk)
+        time.sleep(1)
+        tools.mount(Pgm_env.data_disk,Pgm_env.data_path)
+        try:
+            while (not self.tesk_queue.empty()):
+                data = self.tesk_queue.get()
+                print("开始处理任务:{}".format(data))
+                Pgm_env.log("info","开始处理{}".format(data))
+                tesk = data["tesk"]
+                if(tesk=="dow"):
+                    t = self.ftp.dow(data["data"])
+                    for i in t["ok"]:
+                        i["id"] = data["id"]
+                        mqtt_client.publish(Pgm_env.config["theme"]["r_topic_ok"],json.dumps(i),0)
+                    for i in t["err"]:
+                        i["id"] = data["id"]
+                        mqtt_client.publish(Pgm_env.config["theme"]["r_topic_err"],json.dumps(i),0)
+                    self.re_disk()
+                if(tesk=="getlist"):
+                    for i in data["data"]:
+                        if(os.path.splitext(i)[-1]==""):
+                            l = i.split("/")
+                            path = Pgm_env.data_path
+                            for i in l:
+                                path = os.path.join(path,i)
+                            if(not os.path.exists(path)):
+                                print("不存在这个文件夹")
+                                r = {"code":-1,"re":"不存在这个文件夹"}
+                                mqtt_client.publish(Pgm_env.config["theme"]["r_topic_err"],json.dumps(r),0)
+                                return
+                            list = os.listdir(path)
+                            r = {"code":0,"re":str(list)}
+                            print(r)
+                            mqtt_client.publish(Pgm_env.config["theme"]["r_topic_ok"],json.dumps(r),0)
+                        else:
+                            r = {"code":-1,"re":"不是一个文件夹"}
+                            print("不是一个文件夹")
+                            mqtt_client.publish(Pgm_env.config["theme"]["r_topic_err"],json.dumps(r),0)
+                if(tesk=="info"):
+                    pass
+                if(tesk == "up"):
+                    t = self.ftp.up(data["data"])
+                    mqtt_client.publish(Pgm_env.config["theme"]["r_topic_ok"],json.dumps(t),0)
+                
+                Pgm_env.log("info","处理完毕{}".format(data))
+                print("任务处理完毕:{}".format(data))
+                
+        except Exception as e:
+            # pass
+            print(e.args)
+        finally:
+            print("当前任务处理完毕")
+            Pgm_env.thread_status = 0
+
+    def re_disk(self):
+        tools.log_move(os.path.join(Pgm_env.pgm_path,"log"),Pgm_env.data_path)
+        tools.rmmod()
+        tools.insmod(Pgm_env.data_disk)
+####################class end####################
+        
+#####################func########################
+
+# mqtt链接回调函数
+def on_connect(client, userdata, flags, rc):
+    if 0==rc:
+        print("连接成功")
+        Pgm_env.log("info","mqtt连接成功")
+    elif 1==rc:
+        print("连接失败-不正确的协议版本")
+        Pgm_env.log("error","mqtt连接失败-不正确的协议版本")
+    elif 2==rc:
+        print("连接失败-无效的客户端标识符")
+        Pgm_env.log("error","mqtt连接失败-无效的客户端标识符")
+    elif 3==rc:
+        print("连接失败-服务器不可用")
+        Pgm_env.log("error","连接失败-服务器不可用")
+    elif 4==rc:
+        print("连接失败-错误的用户名或密码")
+        Pgm_env.log("error","mqtt连接失败-错误的用户名或密码")
+    elif 5==rc:
+        print("连接失败-未授权")
+        Pgm_env.log("error","mqtt连接失败-未授权")
+    else:
+        print("6-255: 未定义.")
 
 def on_message(client, userdata, msg):
-    tesk = msg.payload.decode('gb2312')
+    tesk = msg.payload.decode('utf-8')
     try:
+        Pgm_env.log("info","mqtt接受:{}".format(tesk))
         tesk = json.loads(tesk)
     except json.decoder.JSONDecodeError:
-        mqtt_client.publish(config["theme"]["r_topic_err"], "mqtt订阅主题传入参数异常")
+        Pgm_env.log("error","mqtt订阅主题传入参数异常")
         print("mqtt订阅主题传入参数异常")
+        client.publish(Pgm_env.config["theme"]["r_topic_err"],"mqtt订阅主题传入参数异常",0)
         return
     tesk_queue.put(tesk)
-    if(not p_data["tesk_thread_status"]):
-        ftp_config = config["ftp-server"]
-        
-        ftp = MyFtp.MyFtp(ftp_config["host"],int(ftp_config["port"]),ftp_config["user"],ftp_config["pwd"])
-        thread = TeskThread.TeskThread(mqtt_client,tesk_queue,p_data,ftp)
+
+    ftp_config = Pgm_env.config["ftp-server"]
+    ftp = Myftp(ftp_config["host"],int(ftp_config["port"]),ftp_config["user"],ftp_config["pwd"])
+    t = 0
+    n = ftp.connect()
+    while (n["code"]!=0 and t <10):
+        t+=1
+        time.sleep(1)
+        n = ftp.connect()
+        Pgm_env.log("error","ftp链接失败在尝试:{}次".format(str(t)))
+        print("ftp链接失败在尝试:{}次".format(str(t)))
+    t = 0
+    if(n["code"]!=0):
+        Pgm_env.log("error","ftp尝试链接失败建议重启设备并且查看配置文件")
+        client.publish(Pgm_env.config["theme"]["r_topic_err"],"ftp尝试链接失败,建议重启并查看配置文件",0)
+        return
+    if(not Pgm_env.thread_status):
+        thread = TeskThread(mqtt_client,tesk_queue,ftp)
         thread.start()
-    p_data["tesk_thread_status"] = 1
+    Pgm_env.thread_status = 1
 
 
+def on_disconnect(client, userdata, rc):
+    if rc!=0:
+        try:
+            Pgm_env.log("error","mqtt已断开正在尝试重新链接")
+        except socket.error:
+            Pgm_env.log("error","mqtt重连失败")
+    Pgm_env.log("info","mqtt重新链接成功")
 
-def subscribe(theme):
-    theme = theme.split(",")
-    for i in theme:
-        mqtt_client.subscribe(i,0)    
-def  config_v(list):
-    t1 = tools.get_config("config.ini")
+
+def config_v(list):
+    t1 = tools.get_config("./config/defconfig.ini")
     for i in t1.keys():
         if(i not in list):
             return False
@@ -45,24 +337,60 @@ def  config_v(list):
             if(j not in list[i]):
                 return False
     return True
-p_data = {"data_path":"/root/run/data","tesk_thread_status":0}
-if(__name__ == "__main__"):
-    # 读取配置文件
-    config = tools.get_config(os.path.join(p_data["data_path"],"config.ini"))
-    # 判断配置文件是否正常
-    if(not config_v(config)):
-        print("配置文件不正常将实例配置文件复制到U盘进行展示")
-        shutil.copy("config.ini",p_data["data_path"])
-        sys.exit(-1)
-    p_data["config"] = config
-    # 配置mqtt
-    mqtt_client = mqttclient.Client(tools.get_mac())
-    mqtt_client.on_message = on_message
-    mqtt_client.username_pw_set(config["mqtt-server"]["user"],config["mqtt-server"]["pwd"])
-    mqtt_client.connect(config["mqtt-server"]["host"],int(config["mqtt-server"]["port"]),5)
-    mqtt_client.loop_start()
-    subscribe(config["theme"]["w_topic"])
-    while True:
-        pass
 
+def subscribe(cli,theme):
+    theme = theme.split(",")
+    for i in theme:
+        cli.subscribe(i,0)
+        Pgm_env.log("info","订阅主题:{}".format(i))
+
+if __name__ == "__main__":
+    tools.umount(Pgm_env.data_disk)
+    tools.mount(Pgm_env.data_disk,Pgm_env.data_path)
+
+    mac = tools.get_mac()
+    with open(os.path.join(Pgm_env.data_path,"mac.txt"),"w",encoding='utf-8') as f:
+        f.writelines(mac)
+    ip = tools.get_ip()
+    with open(os.path.join(Pgm_env.data_path,"ip.txt"),"w",encoding='utf-8') as f:
+        f.writelines(ip)
+
+    tools.rmmod()
+    tools.insmod(Pgm_env.data_disk)
+
+    Pgm_env.log = Log(Pgm_env.pgm_path).log
+    if(os.path.exists(os.path.join(Pgm_env.data_path,"config.ini"))):
+        shutil.move(os.path.join(Pgm_env.data_path,"config.ini"),os.path.join(Pgm_env.pgm_path,"config"))
+        Pgm_env.log("info","设置IP--需要重启")
+        s = tools.get_config(os.path.join(Pgm_env.pgm_path,"config/config.ini"))
+        s = s["upan"]
+        tools.set_ip(s)
+        print(s)
+
+
+    if(not os.path.exists(os.path.join(Pgm_env.pgm_path,"config/config.ini"))):
+        Pgm_env.log("error","配置文件不存在,将默认配置文件写入U盘--修改完配置文件重启")
+        shutil.copyfile(os.path.join(Pgm_env.pgm_path,"config/defconfig.ini"),os.path.join(Pgm_env.data_path,"config.ini"))
+        tools.rmmod()
+        tools.insmod(Pgm_env.data_disk)
+        sys.exit(-1)
+
+    Pgm_env.config = tools.get_config(os.path.join(Pgm_env.pgm_path,"config/config.ini"))
+    if(not config_v(Pgm_env.config)):
+        Pgm_env.log("error","配置文件有误")
+        tools.rmmod()
+        tools.insmod(Pgm_env.data_disk)
+        sys.exit(-1)
+
+
+
+    mqtt_client = mqttclient.Client(tools.get_mac(),clean_session = False)
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+    Pgm_env.mqtt_client = mqtt_client
+    mqtt_client.username_pw_set(Pgm_env.config["mqtt-server"]["user"],Pgm_env.config["mqtt-server"]["pwd"])
+    mqtt_client.connect(Pgm_env.config["mqtt-server"]["host"],int(Pgm_env.config["mqtt-server"]["port"]),60)
+    # mqtt_client.loop_start()
+    subscribe(mqtt_client,Pgm_env.config["theme"]["w_topic"])
+    mqtt_client.loop_forever()
     
--- tools.py
+++ tools.py
@@ -1,20 +1,36 @@
 import os
-import sys
 import uuid
 import subprocess
-import psutil
 import configparser
+import socket
+import shutil
 config = configparser.ConfigParser()
 
-def insmod_ko(file):
-    out_bytes = subprocess.check_output(['uname','-r']).decode('utf-8').replace("\n","")
-    os.system("insmod /lib/modules/{}/kernel/drivers/usb/gadget/legacy/g_mass_storage.ko file={} stall=0 removable=1".format(out_bytes,file))
 def rmmod():
+    os.system("sync")
     os.system("rmmod g_mass_storage")
-def mount_fdisk(fdisk,path):
-    os.system("mount {} {}".format(fdisk,path))
-def umount_fdisk(fdisk):
-    os.system("umount {}".format(fdisk))
+    os.system("sync")
+
+def insmod(fdisk):
+    os.system("sync")
+    out_bytes = subprocess.check_output(['uname','-r']).decode('utf-8').replace("\n","")
+    os.system("insmod /lib/modules/{}/kernel/drivers/usb/gadget/legacy/g_mass_storage.ko file={} stall=0 removable=1".format(out_bytes,fdisk))
+    os.system("sync")
+def get_mac():
+    mac_address = uuid.UUID(int=uuid.getnode()).hex[-12:].upper()
+    mac_address = '-'.join([mac_address[i:i+2] for i in range(0, 11, 2)])
+    return mac_address
+
+def umount(disk):
+    os.system("sync")
+    os.system("umount {}".format(disk))
+    os.system("sync")
+
+
+def mount(disk,path):
+    os.system("sync")
+    os.system("mount {} {}".format(disk,path))
+    os.system("sync")
 
 def get_config(file):
     config.read(file)
@@ -26,21 +42,32 @@ def get_config(file):
         list[i] = s
     return list
 
-def get_mac():
-    mac =  hex(uuid.getnode())[2:].zfill(12)
-    return mac
-
-def get_dirlist(path):
-    if(not os.path.exists(path)):
-        return {"code":-1,"Re":"本地文件夹不存在","data":'[]'}
+def set_ip(d):
+    print(d)
+    if(d["ip"])=="0.0.0.0":
+        text = "auto eth0\niface eth0 inet dhcp\n"
+        with open("/etc/network/interfaces.d/usereth0","w",encoding="utf-8") as f:
+            f.writelines(text)
     else:
-        list = os.listdir(path)
-        return {"code":0,"Re":"ok","data":str(list)}
-    
-def getinfo():
-    disk = psutil.disk_usage("/dev/mmcblk0p4")
-    cpu = psutil.cpu_count()
-    ram = psutil.virtual_memory()
-    l = {"CPU":cpu,"disk":disk,"ram":ram}
-    r = {"code":0,"Re":"ok","data":str(l)}
-    return r
+        text = "auto eth0\niface eth0 inet static\naddress {}\nnetmask {}\ngateway {}\nbroadcast {}\n".format(d["ip"],d["subnetmask"],d["interway"],d["broadcast"])
+        with open("/etc/network/interfaces.d/usereth0","w",encoding="utf-8") as f:
+            f.writelines(text)
+
+
+
+def get_ip():
+    ip = "127.0.0.1"
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+    return ip
+
+def log_move(src,dst):
+    os.sync()
+    for i in os.listdir(src):
+        path = os.path.join(src,i)
+        shutil.copy(path,dst)
+    os.sync()
